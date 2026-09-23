2d22a62887099c5bbdf2a577e26026d44d91331a
Florent.Bourgeois <florent.bourgeois@uha.fr>
2026-09-23 22:59:20 +0200

    initial commit

---
+++ src/main/java/fr/ensisa/ensisa2026/Ensisa2026Application.java
@@ -0,0 +1,13 @@
+package fr.ensisa.ensisa2026;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ensisa2026Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ensisa2026Application.class, args);
+    }
+
+}
---
+++ src/test/java/fr/ensisa/ensisa2026/Ensisa2026ApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.ensisa.ensisa2026;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ensisa2026ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
